6f34dfc2a44383cd289b1f71967a889a2e3ac77b
Abhishek Kumar <154977045+BcaAbhishek@users.noreply.github.com>
2026-03-27 14:48:51 +0530

    Delete backend directory

--- backend/models/History.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const historySchema = new mongoose.Schema({
-userId: String,
-data: Object,
-createdAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model("History", historySchema);
